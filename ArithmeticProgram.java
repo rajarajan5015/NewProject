6b05ae30dc38c27de9d51825ca0d5a8b0cf2318e
rajarajan5015 <rr947997@gmail.com>
2023-03-20 17:07:42 +0530

    new change

--- ArithmeticProgram.java
+++ ArithmeticProgram.java
@@ -5,10 +5,12 @@ public class ArithmeticProgram {
 		int number1=10;
 		int number2=2;
 		int add= number1+number2;
+		int add2=number1+number2;
 		int subraction=number1-number2;
 		int multiplication=number1*number2;
 		int divided=number1/number2;
 		int modulus=number1%number2;
+		System.out.println("add2="+add2);
 		System.out.println("modulus="+modulus);
 		System.out.println("multiplication="+multiplication);
 	
